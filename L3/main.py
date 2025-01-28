acb4eb101182862657a6a1be7b572445882893dc
Saparmurat <saparmurat.nurlan@gmail.com>
2025-01-28 19:15:20 +0600

    print lines from text file

--- L3/main.py
+++ L3/main.py
@@ -1,6 +1,33 @@
-name = input("Enter your name: ")
+line_to_append = ""
 
-print("Hello, " + name)
+while not line_to_append:
+    line_to_append = input("Enter line to append: ")
+
+number_of_lines = 0
+
+while number_of_lines <= 0:
+    initial_input = input("Enter the number of lines: ")
+
+    if not initial_input.isdigit() and not (initial_input[0] == '-' and initial_input[1:].isdigit()):
+        print("WARNING: " + initial_input + " is not a number!")
+        continue
+    
+    # TODO: give warning for negative numbers and 0
+
+    number_of_lines = int(initial_input)
 
 
-file = open("data.txt")
+print("Line to append: " + line_to_append)
+print("Number of lines to print: " + str(number_of_lines))
+
+file = open("C:/Users/Saparmurat/Desktop/Python/L3/data.txt", "r+")
+
+
+for i in range(number_of_lines):
+    line = file.readline()
+    print(line, end='')
+
+
+file.write(line_to_append + "\n")
+
+file.close()
