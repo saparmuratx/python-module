76ec58cdeff603c665410092256905f878244854
Saparmurat <saparmurat.nurlan@gmail.com>
2025-02-11 10:53:12 +0600

    add L5

---
+++ L5/main.py
@@ -0,0 +1,19 @@
+my_list = [1, 2, 3]
+my_tuple = (1, -2, my_list)
+
+print(my_list)
+print(my_tuple)
+
+
+another_list = ["abc", "qwerty"]
+
+def get_tuple():
+    return 1,
+
+print(get_tuple())
+print(type(get_tuple()))
+
+new_tuple = list(range(1,3 + 1))
+
+print(new_tuple)
+print(type(new_tuple))
